c95004b81b5f1e107ac5dd6172ff6f4bab0bc619
Will Maier <willmaier@ml1.net>
2011-01-22 15:28:06 -0600

    Initialize slides.

--- js/talks.js
+++ js/talks.js
@@ -1,17 +1,40 @@
 (function ($) {
     var defaults = {
     };
+    var slides = [];
+    var current = 0;
+    var invisible = "slides-invisible";
 
-    function slides (element, settings) {
+    function initslides (element, settings) {
         var root = $(element);
-        root.addClass("slides");
+        var header = root.find("> header");
+        var footer = root.find("> footer");
+
+        /* Make header, footer and slides invisible. */
+        header.addClass(invisible);
+        footer.addClass(invisible);
+        root.addClass("slides").children("section").each(function () {
+            var slide = $(this);
+            slide.addClass(invisible);
+            slides.push($(this));
+        });
+
+        /* 
+         * If any slides are present, make the header, footer and current slide
+         * visible again
+         */
+        if (slides) {
+            header.removeClass(invisible);
+            footer.removeClass(invisible);
+            slides[current].removeClass(invisible);
+        };
     };
 
     $.fn.slides = function (options) {
         var settings = $.extend({}, defaults, options);
 
         return this.each(function () {
-            slides(this, settings);
+            initslides(this, settings);
         });
     };
 })(jQuery);
