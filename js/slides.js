63e0ef243096ac643c274fdc8de640533a6a9841
Will Maier <willmaier@ml1.net>
2011-01-23 09:33:11 -0600

    Use show/hide to transition slides.

--- js/slides.js
+++ js/slides.js
@@ -1,5 +1,6 @@
 (function ($) {
     var defaults = {
+        duration: 0,
         bindings: {
             8: prevslide,  // backspace
             37: prevslide, // left
@@ -19,27 +20,26 @@
     var slides = [];
     var root, header, footer;
     var current = 0;
-    var invisible = "slides-invisible";
 
     function initslides (element) {
         root = $(element);
         header = root.find("> header");
         footer = root.find("> footer");
 
-        /* Make header, footer and (slides > 1) invisible. */
+        /* Hide header, footer and (slides > 1). */
         root.addClass("slides").children("section").each(function () {
             slides.push($(this));
         });
         $.each(slides.slice(1), function () {
-            $(this).addClass(invisible);
+            $(this).hide();
         });
     };
 
     function gotoslide (index) {
         if (index >= slides.length || index < 0)
             return;
-        slides[current].addClass(invisible);
-        slides[index].removeClass(invisible);
+        hideslide(slides[current]);
+        showslide(slides[index]);
         current = index;
     };
 
@@ -50,6 +50,14 @@
     function prevslide () {
         gotoslide(current - 1);
     }
+
+    function showslide (slide) {
+        slide.show(settings.duration);
+    };
+
+    function hideslide (slide) {
+        slide.hide(settings.duration);
+    };
 
     $.fn.slides = function (options) {
         $.extend(settings, defaults, options);
