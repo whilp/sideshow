fa53fc7968cf3ebfcd575ec4b8200fa09d480879
Will Maier <willmaier@ml1.net>
2011-01-23 10:02:58 -0600

    Restructure.

--- js/slides.js
+++ js/slides.js
@@ -1,13 +1,39 @@
 (function ($) {
-    var settings = {};
-    var slides = [];
-    var root, header, footer;
-    var current = 0;
+    $.fn.slides = function (options) {
+        $.extend($.fn.slides.settings, $.fn.slides.defaults, options);
 
-    function initslides (element) {
-        root = $(element);
-        header = root.find("> header");
-        footer = root.find("> footer");
+        return this.each(function () {
+            $.fn.slides.init(this);
+        });
+    };
+
+    $.fn.slides.settings = {},
+    $.fn.slides.slides = [];
+    $.fn.slides.current = 0;
+
+    $.fn.slides.defaults = {
+        duration: 0,
+        bindings: {
+            8:  "prev", // backspace
+            37: "prev", // left
+            38: "prev", // up
+            39: "next", // right
+            40: "next", // down
+            72: "prev", // h
+            75: "prev", // k
+            74: "next", // j
+            76: "next", // l
+            78: "next", // n
+            80: "prev", // p
+            32: "next", // space
+        },
+    };
+
+    $.fn.slides.init = function (element) {
+        $.fn.slides.root = root = $(element);
+        $.fn.slides.header = header = root.find("> header");
+        $.fn.slides.footer = footer = root.find("> footer");
+        var slides = $.fn.slides.slides;
 
         /* Hide header, footer and (slides > 1). */
         root.addClass("slides").children("section").each(function () {
@@ -18,64 +44,40 @@
         });
     };
 
-    function gotoslide (index) {
+    $.fn.slides.goto = function (index) {
+        var slides = $.fn.slides.slides,
+            current = $.fn.slides.current,
+            settings = $.fn.slides.settings;
         if (index >= slides.length || index < 0)
             return;
-        settings.hide(slides[current]);
-        settings.show(slides[index]);
-        current = index;
+        $.fn.slides.hide(slides[current]);
+        $.fn.slides.show(slides[index]);
+        $.fn.slides.current = index;
     };
 
-    function nextslide () {
-        gotoslide(current + 1);
+    $.fn.slides.next = function () {
+        $.fn.slides.goto($.fn.slides.current + 1);
     }
 
-    function prevslide () {
-        gotoslide(current - 1);
+    $.fn.slides.prev = function () {
+        $.fn.slides.goto($.fn.slides.current - 1);
     }
 
-    function showslide (slide) {
-        slide.show(settings.duration);
+    $.fn.slides.show = function (slide) {
+        slide.show($.fn.slides.settings.duration);
     };
 
-    function hideslide (slide) {
-        slide.hide(settings.duration);
-    };
-
-    $.fn.slides = function (options) {
-        $.extend(settings, defaults, options);
-
-        return this.each(function () {
-            initslides(this, settings);
-        });
+    $.fn.slides.hide = function (slide) {
+        slide.hide($.fn.slides.settings.duration);
     };
 
     $(document).keydown(function (event) {
         var key = (event.keyCode ? event.keyCode : event.which);
-        var fn = settings.bindings[key];
+        var fnname = $.fn.slides.settings.bindings[key],
+            fn = $.fn.slides[fnname];
         if (fn) {
             fn();
             return false;
         };
     });
-
-    var defaults = {
-        show: showslide,
-        hide: hideslide,
-        duration: 0,
-        bindings: {
-            8: prevslide,  // backspace
-            37: prevslide, // left
-            38: prevslide, // up
-            39: nextslide, // right
-            40: nextslide, // down
-            72: prevslide, // h
-            75: prevslide, // k
-            74: nextslide, // j
-            76: nextslide, // l
-            78: nextslide, // n
-            80: prevslide, // p
-            32: nextslide, // space
-        },
-    };
 })(jQuery);
