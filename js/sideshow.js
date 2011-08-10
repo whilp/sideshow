408a79597818cc649aa4cff7f783e5bde80598df
Will Maier <willmaier@ml1.net>
2011-08-10 09:51:52 -0500

    Add utility functions.

--- js/sideshow.js
+++ js/sideshow.js
@@ -1,5 +1,34 @@
 var sideshow = function () {
+    function hasClass (elem, cls) {
+        var names = elem.className.split(" ");
+        for (var i = 0; i < names; i++) {
+            if (cls == names[i])
+                return true;
+        }
+        return false;
+    }
+
+    function addClass (elem, cls) {
+        if (!hasClass(elem, cls))
+            elem.className += " " + cls;
+    }
+
+    function removeClass (elem, cls) {
+        var names = elem.className.split(" "),
+        for (var i = 0; i < names.length; i++) {
+            if (names[i] == cls)
+                names.splice(i, 1);
+        }
+        elem.className = names.join(" ");
+    }
+
     function init (root) {
+        var root = document.getElementById(root);
+        var sections = root.getElementsByTag("section");
+
+        for (var i = 0; i < sections.length; i++) {
+            addClass(sections[i], "slide");
+        }
     }
     return {
         init: init,
